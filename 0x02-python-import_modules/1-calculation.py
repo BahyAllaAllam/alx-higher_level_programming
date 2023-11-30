c29d18a25ab2461a19e959336da323d75bf506f1
BahyAllaAllam <bahy.alam@yahoo.com>
2023-11-30 02:21:37 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     division = divide(a, b)
     print("{} + {} = {}".format(a, b, addition))
     print("{} - {} = {}".format(a, b, subtraction))
-    print("{} + {} = {}".format(a, b, multiplication))
-    print("{} + {} = {}".format(a, b, division))
+    print("{} * {} = {}".format(a, b, multiplication))
+    print("{} / {} = {}".format(a, b, division))
