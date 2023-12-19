9ade6c621f6ccc20646d329e70a79956df294ac0
BahyAllaAllam <bahy.alam@yahoo.com>
2023-12-19 00:21:31 +0000

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        unsupported_type = {}
-        unsupported_type[()] = 42
-
-    except TypeError as e:
-        raise e
+    raise TypeError
