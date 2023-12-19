13dfe504f1f731dd5733e099a9b48f15af79e7e2
BahyAllaAllam <bahy.alam@yahoo.com>
2023-12-19 00:01:59 +0000

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,9 +5,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError("out of range")
-
-            dividend = my_list_1[i] if isinstance(my_list_1[i], (int, float)) else None
-            divisor = my_list_2[i] if isinstance(my_list_2[i], (int, float)) else None
+            if isinstance(my_list_1[i], (int, float)):
+                dividend = my_list_1[i]
+            else:
+                dividend = None
+            if isinstance(my_list_2[i], (int, float)):
+                divisor = my_list_2[i]
+            else:
+                divisor = None
 
             if dividend is None or divisor is None:
                 result.append(0)
