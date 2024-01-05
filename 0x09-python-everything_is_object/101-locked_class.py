2ba9f103bdee51142b9e6db1b2c93034b3172896
BahyAllaAllam <bahy.alam@yahoo.com>
2024-01-05 17:26:57 +0000

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Defines LockedClass that only allowed to create the first name instance
+"""
+
+
 class LockedClass:
+    """LockedClass that only allowed to create the first name instance"""
+
     __slots__ = ('first_name',)
