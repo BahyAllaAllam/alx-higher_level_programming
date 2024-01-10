d3653b710f9653548f6a2049d29e1dc21a3e60ed
BahyAllaAllam <bahy.alam@yahoo.com>
2024-01-10 02:59:53 +0000

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,13 +11,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 try:
-   items = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 except FileNotFoundError:
-   items = []
+    items = []
 
 # Add all command-line arguments to the list
 for arg in sys.argv[1:]:
-   items.append(arg)
+    items.append(arg)
 
 # Save the updated list back to the file
 save_to_json_file(items, filename)
