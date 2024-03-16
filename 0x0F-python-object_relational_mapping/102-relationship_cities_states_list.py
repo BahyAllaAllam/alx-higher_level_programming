1aea1de54bb76967887f2b9e169ea7f7007c7c69
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-16 14:49:23 -0700

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
-'''
-script that lists all City objects from the database hbtn_0e_101_usa
-'''
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print('Usage: python script_name.py mysql_username '
-              'mysql_password database_name')
-    else:
-        # Extract command-line arguments
-        username, password, database = sys.argv[1:]
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
-        # Create engine and bind session
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                username,
-                password,
-                database
-            ), pool_pre_ping=True
-        )
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        # Query all City objects with their corresponding State objects
-        for city in session.query(City).order_by(City.id):
-            print(
-                "{}: {} -> {}".format(city.id, city.name, city.state.name)
-            )
-
-        # Close session
-        session.close()
+    for city, state in session.query(City, State)\
+                              .join(State, State.id == City.state_id)\
+                              .order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
