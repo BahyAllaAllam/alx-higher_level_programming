a0daa45ed69ee6535e35e9a5c645a7740c001ec9
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-16 15:03:30 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,59 +1,19 @@
 #!/usr/bin/python3
-"""
-This script connects to a MySQL server and
-    displays all values in the states table
-where the name matches the provided argument.
-"""
+"""Lists states"""
 
 import MySQLdb
-import sys
-
-
-def filter_states(username, password, database, state_name):
-    """
-    Connects to the MySQL server and filters states by the provided name.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-        state_name (str): State name to search for.
-
-    Returns:
-        None
-    """
-    try:
-        # Connect to the MySQL server
-        conn = MySQLdb.connect(host='localhost', port=3306, user=username,
-                               passwd=password, db=database)
-        cursor = conn.cursor()
-
-        # Create and execute the SQL query using format with user input
-        query = ("SELECT * FROM states WHERE name = '{}' "
-                 "ORDER BY id ASC;").format(state_name)
-        cursor.execute(query)
-
-        # Fetch all rows and display them
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL database: {e}")
-        sys.exit(1)
-    finally:
-        if conn:
-            conn.close()
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python 2-my_filter_states.py "
-              "<username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-    filter_states(username, password, database, state_name)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
