33eabbf2a87ea0c7062e1187744c5a21f85407b4
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-16 14:43:57 -0700

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -5,7 +5,7 @@ script that lists all City objects from the database hbtn_0e_101_usa
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from relationship_state import Base, State
 from relationship_city import City
 
