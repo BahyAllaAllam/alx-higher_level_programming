9388d8914acd54d28b3b4f0912f0a93df114d5d1
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-28 04:12:36 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,6 @@
 import urllib.request
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: {} <URL>".format(sys.argv[0]))
-    sys.exit(1)
-
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
