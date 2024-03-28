4b8a39ddf95f262246cacc959aaa6f802425d715
BahyAllaAllam <bahy.alam@yahoo.com>
2024-03-28 16:53:38 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,15 +2,13 @@
 """Python script that takes in a URL,
    sends a request to the URL and displays the body of the response."""
 
-
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
+    response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print("Error code:", response.status_code)
-
-    print(response.text)
+    else:
+        print(response.text)
