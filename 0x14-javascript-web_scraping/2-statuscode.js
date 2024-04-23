0bdb5bbc49f7291e1ad3b7c0012e0534d4a84c9f
BahyAllaAllam <bahy.alam@yahoo.com>
2024-04-23 02:34:58 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+  console.log(`code: ${response.statusCode}`);
 });
